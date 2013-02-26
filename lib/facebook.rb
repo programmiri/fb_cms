b24bf440393daa23b5abbf921177ab0b0ebc663f
programmiri <mirjam.aulbach@gmail.com>
2013-02-26 22:36:19 +0100

    comment

--- lib/facebook.rb
+++ lib/facebook.rb
@@ -1,3 +1,4 @@
+#Namespace Facebook / Klasse
 $:.unshift(File.expand_path(File.dirname(__FILE__))) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
